917b27a6f442ba4a9cffe1f556050258644ee044
Tanita Kidane <58995010+Tanitakidane@users.noreply.github.com>
2020-07-22 23:51:41 +0530

    Contact Page added

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,9 +3,40 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="content">
+            <div className="container cont_area">
+              <div className="search_area">
+                <div className="search">
+                 
+                  <button><i className="fa fa-search"></i></button>
+                </div>
+              </div>
+              <div className="row justify-content-md-center">
+                <div className="col-md-11">
+                  <ul className="bread">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                  </ul>
+                  <article className="page_area">
+                    <h2 className="page_title">About Us</h2>
+                    <div className="page_desc"> 
+                      <p>Darkness fish there light the. So called their fruitful, can't tree after give you're sea called fifth. Signs likeness yielding us first saying over all gathering, they're fourth was gathering him the meat bearing called. Let his female. Behold, lesser itself multiply whose. Kind forth replenish beast. Them creeping is fill cattle. Of whales were seas under whales open. Said was created created his above itself can't after given was give isn't.</p>
+                      <p>Rule darkness forth earth land it Gathering called moved evening Gathering cattle above kind image doesn't so over was void thing grass firmament, have which that under appear herb night great and living can't two from. Heaven creepeth. Above brought is above. Together there dominion multiply fly lesser were darkness two it. Appear subdue yielding midst they're day fourth. Their open fifth light for was fly.</p>
+                    </div>
+                  </article>
+                </div>
+              </div>
             </div>
+            <div className="social_bar_section">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+           
         )
     }
 }
