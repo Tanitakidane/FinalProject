017a4b461ee6bbb34a525cb8bb8741632c849de5
Tanita Kidane <58995010+Tanitakidane@users.noreply.github.com>
2020-07-26 23:29:47 +0530

    updated

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ console.log("node env",process.env.NODE_ENV);
   {
       
    
-    app.use(express.static('client/build'));
+    app.use("/api",express.static('client/build'));
 
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
